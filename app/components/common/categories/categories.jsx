e3ea781c71ee0ba27017c1193692ca1f1174298d
nurullah <nurullah7733@gmail.com>
2024-11-24 20:38:26 +0600

    categoryItem path fix

--- app/components/common/categories/categories.jsx
+++ app/components/common/categories/categories.jsx
@@ -1,6 +1,6 @@
 "use client";
 import React, { useState } from "react";
-import CategoryItem from "./CategoryItem";
+import CategoryItem from "./categoryItem";
 
 const Categories = ({ categories }) => {
   // console.log(categories, "categories");
